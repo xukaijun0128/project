d5da48b4c944c721e3ea4f708c0b898dd7b1deea
胥凯俊 <xukaijun0128@sina.com>
2016-03-08 13:59:41 +0800

    js面对对象编程

--- Public/Js/admin/admin.js
+++ Public/Js/admin/admin.js
@@ -1,79 +1,84 @@
-var systemConfig = Backbone.View.extend({
-    el: $('#systemDiv'),
-    events: {
-        'click .add': 'addInfo',
-        'click .edit': 'editInfo',
-        'click .reset': 'resetInfo'
-    },
-    initialize: function() {
-        this.table = this.$('.systemTable');
-        this.render();
-    },
-    render: function() {
-        var _this = this;
-        $.each(_this.getConfig(), function(k,v) {
-            _this.table.append(
-                "<tr>" +
-                    "<td style='width:265px;'>" +
-                        "<div class='cut_title ellipsis'>" + v.parameter + "</div>" +
-                    "</td>" +
-                    "<td class='info'><input type='text' value="+v.value+" id="+v.id+"></td>" +
-                    "<td>"+ v.variable +"</td>" +
-                "</tr>"
-            );
-        });
-    },
-    getConfigInfo: function() {
-        
-    },
-    addInfo: function() {
-        
-    },
-    editInfo: function() {
-        var _this = this,
-            id = '',
-            data,
-            value = '',
-            datas = [];
-        this.table.find('tr').each(function(k, v) {
-            data = {};
-            id = $(v).children('td.info').children().prop('id');
-            value = $(v).children('td.info').children().prop('value');
-            if (typeof id !== 'undefined') {
-                data['id'] = id;
-                data['value'] = value;
-                datas.push(data);
-            }
-        });
-        datas = JSON.stringify(datas);
-        $.ajax({
-            type: "GET",
-            url: '/admin/System/editConfig',
-            async: false,
-            data: {data: datas},
-            dataType: "json",
-            success: function(data) {
-                alert('修改成功');
-            }
-        });
-    },
-    resetInfo: function() {
-        
-    },
-    getConfig: function() {
-        var configData = [];
-        $.ajax({
-            type: "GET",
-            url: '/admin/System/getConfig',
-            async: false,
-            dataType: "json",
-            success: function(data) {
-                configData = data;
-            }
-        });
-        return configData;
-    }
+var exports = {
+    tpl: _.template(
+                '<% _.each(config, function(item) {%>' +
+                '<tr>' +
+                    '<td style="width:265px">' +
+                        '<div class="cut_title ellipsis"><%= item.parameter %></div>' +
+                    '</td>' +
+                    '<td class="info"><input type="text" value=<%= item.value %> id=<%= item.id %>>' +
+                    '</td>' +
+                    '<td><%= item.variable %></td>' +
+                '</tr>' +
+              '<% }) %>'
+        ),
+    systemConfig: Backbone.View.extend({
+        el: $('#systemDiv'),
+        events: {
+            'click .add': 'addInfo',
+            'click .edit': 'editInfo',
+            'click .reset': 'resetInfo'
+        },
+        initialize: function() {
+            var tap = '<h1><%=width%></h1>';
+            this.$el.append(_.template(tap, {width: '我的个大擦'}));
+            this.table = this.$('.systemTable');
+            this.render();
+        },
+        render: function() {
+            var _this = this;
+            _this.table.append(exports.tpl({config: _this.getConfig()}));
+        },
+        getConfigInfo: function() {
 
-});
+        },
+        addInfo: function() {
 
-new systemConfig;
+        },
+        editInfo: function() {
+            var _this = this,
+                id = '',
+                data,
+                value = '',
+                datas = [];
+            this.table.find('tr').each(function(k, v) {
+                data = {};
+                id = $(v).children('td.info').children().prop('id');
+                value = $(v).children('td.info').children().prop('value');
+                if (typeof id !== 'undefined') {
+                    data['id'] = id;
+                    data['value'] = value;
+                    datas.push(data);
+                }
+            });
+            datas = JSON.stringify(datas);
+            $.ajax({
+                type: "GET",
+                url: '/admin/System/editConfig',
+                async: false,
+                data: {data: datas},
+                dataType: "json",
+                success: function(data) {
+                    alert('修改成功');
+                }
+            });
+        },
+        resetInfo: function() {
+
+        },
+        getConfig: function() {
+            var configData = [];
+            $.ajax({
+                type: "GET",
+                url: '/admin/System/getConfig',
+                async: false,
+                dataType: "json",
+                success: function(data) {
+                    configData = data;
+                }
+            });
+            return configData;
+        }
+    })
+};
+
+new exports.systemConfig;
